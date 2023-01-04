b8b3457b002b9f61d4d961fbb6bb754b0d24abf9
sehyeon <hyeon4687@naver.com>
2023-01-04 22:31:09 +0900

    페이지 분리

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from './Component/Footer';
+import Header from './Component/Header';
+import Body from './Component/Body';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          반갑습니다 :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Body />
+    <Footer />
+    </>
   );
 }
 
