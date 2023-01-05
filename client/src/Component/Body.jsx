de90a8a116124eeae3cb439ca0887fc5a783da18
sehyeon <hyeon4687@naver.com>
2023-01-05 09:01:46 +0900

    오류수정

--- client/src/Component/Body.jsx
+++ client/src/Component/Body.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Main from '../Page/Main'
-import Login from '../Page/Login'
 import { Route, Routes } from 'react-router-dom'
 import List from '../Page/List'
 
@@ -10,7 +9,6 @@ const Body = () => {
     <div>
         <Routes>
             <Route path='/' element={<Main />} />
-            <Route path='/login' element={<Login />} />
             <Route path='/list' element={<List />} />
         </Routes>
     </div>
