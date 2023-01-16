9f06f37ced7812b6c5a42b8fe815ac7a30e06aec
sehyeon <hyeon4687@naver.com>
2023-01-16 14:33:20 +0900

    state 설명

--- client/src/Components/Body.tsx
+++ client/src/Components/Body.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from '../Page/Login'
 import Main from '../Page/Main'
+import styled from "styled-components";
 
 const Body = () => {
   return (
+    <BodyStyle>
     <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/login' element={<Login />} />
     </Routes>
+    </BodyStyle>
   )
 }
 
 export default Body
+
+const BodyStyle = styled.div`
+`
